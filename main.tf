211fadbec18bda8fdb3440c5b82eb7455c0a4c26
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-07-30 20:58:14 +0530

    eks cluster setup

--- main.tf
+++ main.tf
@@ -25,7 +25,35 @@ resource "aws_iam_role_policy_attachment" "main-AmazonEKSVPCResourceController" 
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.main.name
 }
+resource "aws_iam_role" "node" {
+  name = "${local.name}-node-role"
 
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node.name
+}
 resource "aws_eks_cluster" "main" {
   name     = "${var.env}-eks"
   role_arn = aws_iam_role.main.arn
@@ -34,3 +62,21 @@ resource "aws_eks_cluster" "main" {
     subnet_ids = var.subnets_ids
   }
 }
+
+resource "aws_eks_node_group" "node" {
+  for_each        = var.node_groups
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${local.name}-${each.key}-ng"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = var.subnets_ids
+
+  scaling_config {
+    desired_size = lookup(each.value,"size",null)
+    max_size     = each.value["size"]+5
+    min_size     = lookup(each.value,"size",null)
+    instance_types = lookup(each.value,"instance_types",null)
+    capacity_type  = lookup(each.value,"capacity_type",null)
+  }
+
+}
+
--- variables.tf
+++ variables.tf
@@ -2,3 +2,4 @@ variable "env" {}
 variable "project_name" {}
 variable "component" {}
 variable "subnets_ids" {}
+variable "node_groups" {}
