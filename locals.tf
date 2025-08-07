ed45e27c2d0216b69f7e9b9d6d86c520356836e3
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-08-07 13:30:10 +0530

    creating oidc provider

--- locals.tf
+++ locals.tf
@@ -1,3 +1,5 @@
 locals {
   name = "${var.env}-${var.project_name}-${var.component}"
+  # issuer = aws_eks_cluster.main.identity[0].oidc[0].issuer
+  # cluster_issuer_id = split("/", local.issuer)[4]
 }
--- main.tf
+++ main.tf
@@ -129,4 +129,8 @@ resource "aws_eks_node_group" "node" {
   }
 
 
-
+resource "aws_iam_openid_connect_provider" "default" {
+  url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
+  client_id_list  = [ "sts.amazonaws.com" ]
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
+}
